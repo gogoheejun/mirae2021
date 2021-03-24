899cd9f431de187d38121b3293b46ab603755780
heejjuunn <heejjuunn@gmail.com>
2021-03-24 16:51:30 +0900

    24th

--- js_yang/kid.js
+++ js_yang/kid.js
@@ -1,6 +1,8 @@
 const toggleBtn = document.querySelector('#burger');
 const menu = document.querySelector('.header_list');
 
+
 toggleBtn.addEventListener('click',()=>{
     menu.classList.toggle('active');
+
 })
