c3808e2abde241220fabd04d26b196e23777ff67
heejjuunn <heejjuunn@gmail.com>
2021-03-26 01:03:42 +0900

    ok

--- js_yang/kid.js
+++ js_yang/kid.js
@@ -1,8 +1,24 @@
-// const toggleBtn = document.querySelector('#burger');
-// const menu = document.querySelector('.header_list');
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    // loop: true,
+    slidesPerView: 2,
+    spaceBetween: 30,
+    freeMode: true,
 
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
 
-// toggleBtn.addEventListener('click',()=>{
-//     menu.classList.toggle('active');
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
 
-// })
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+    });
