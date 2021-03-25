f3cd92e895125f7e2b17b6bf687bf5e4ae0e64fe
heejjuunn <heejjuunn@gmail.com>
2021-03-25 16:48:54 +0900

    side bar, blurred

--- js_yang/kid.js
+++ js_yang/kid.js
@@ -1,8 +1,8 @@
-const toggleBtn = document.querySelector('#burger');
-const menu = document.querySelector('.header_list');
+// const toggleBtn = document.querySelector('#burger');
+// const menu = document.querySelector('.header_list');
 
 
-toggleBtn.addEventListener('click',()=>{
-    menu.classList.toggle('active');
+// toggleBtn.addEventListener('click',()=>{
+//     menu.classList.toggle('active');
 
-})
+// })
